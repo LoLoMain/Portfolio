84902a4bbf34daf297d054cc7b4295f421156758
Lauren Little <littlelauren016@gmail.com>
2017-08-05 15:55:24 -0400

    updated flip card functionality

---
+++ public/javascripts/other.js
@@ -0,0 +1,11 @@
+// $(document).ready(function() {
+//
+// $("#card").flip();
+//
+// });
+//
+// // axis: 'x',
+// // trigger: 'hover'
+//
+// separate function from above - use either or
+// document.getElementById("#myCard").classList.toggle("flip");
