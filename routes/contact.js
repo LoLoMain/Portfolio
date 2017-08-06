bf5246349bdbf783a8d21f3c5fc9662015adf9d2
Lauren Little <littlelauren016@gmail.com>
2017-08-06 18:50:14 -0400

    added contact-routes functionality

--- routes/contact.js
+++ routes/contact.js
@@ -15,6 +15,27 @@ router.post('/contact', (req, res, next)=>{
     contactEmail: req.body.email,
     contactMessage: req.body.message
   });
+
+  // Save Contact Form Info
+   newContact.save((err)=>{
+     //DB Error
+     if (err && newContact.errors === undefined){
+       next(err);
+       return;
+     }
+     // Valiation errors
+      if(err && newContact.errors){
+        //Variables with the error messages
+        res.locals.nameValidationError = addedClass.errors.contactName;
+        res.locals.emailValidationError = addedClass.errors.contactEmail;
+        res.locals.messageValidationError = addedClass.errors.contacMessage;
+       //Display form to correct errors
+        res.render('/contact');
+        return;
+      }
+      //Save successful
+      res.redirect('/');
+   });
 });
 
 module.exports = router;
